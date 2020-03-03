dd2aa3c66e5c04bd19c0684a4013760d07e714de
Colepa12 <Colepa12@gmail.com>
2020-03-02 22:03:56 -0600

    Started Employee Detail server side

---
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
@@ -0,0 +1,25 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/employeeDetail")
+public class EmployeeDetailRouteController
+{
+    // @RequestMapping(method = RequestMethod.GET)
+    // public ModelAndView start(
+    //     @RequestParam final Map<String, String> queryParameters, 
+    //     final HttpServletRequest request)
+    // {
+        
+    // }
+    
+}
